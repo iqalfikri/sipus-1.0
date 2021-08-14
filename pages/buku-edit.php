f31cdb4608d8e9f6bca205b47ff0389f09a1457a
iqalfikri <haiqal@mexious.com>
2021-08-14 14:11:33 +0700

    adding sipus1.0

---
+++ pages/buku-edit.php
@@ -0,0 +1,62 @@
+<?php
+$id_buku = $_GET['id'];
+$q_tampil_buku = mysqli_query($db, "SELECT * FROM tbbuku WHERE idbuku='$id_buku'");
+$r_tampil_buku = mysqli_fetch_array($q_tampil_buku);
+?>
+<div id="label-page">
+    <h3>Input Data Buku</h3>
+</div>
+<div id="content">
+    <form action="proses/buku-edit-proses.php" method="post">
+        <table id="tabel-input">
+            <tr>
+                <td class="label-formulir">ID Buku</td>
+                <td class="isian-formulir"><input type="text" name="id_buku"
+                        value="<?php echo $r_tampil_buku['idbuku']; ?>" readonly="readonly"
+                        class="isian-formulir isian-formulir-border warna-formulir-disabled"></td>
+            </tr>
+            <tr>
+                <td class="label-formulir">Judul Buku</td>
+                <td class="isian-formulir"><input type="text" name="judul"
+                        value="<?php echo $r_tampil_buku['judulbuku']; ?>" class="isian-formulir isian-formulir-border">
+                </td>
+            </tr>
+            <tr>
+                <td class="label-formulir">Kategori</td>
+                <td class="isian-formulir"><input type="text" name="kategori"
+                        value="<?php echo $r_tampil_buku['kategori']; ?>" class="isian-formulir isian-formulir-border">
+                </td>
+            </tr>
+            <tr>
+                <td class="label-formulir">Pengarang</td>
+                <td class="isian-formulir"><input type="text" name="pengarang"
+                        value="<?php echo $r_tampil_buku['pengarang']; ?>" class="isian-formulir isian-formulir-border">
+                </td>
+            </tr>
+            <tr>
+                <td class="label-formulir">Penerbit</td>
+                <td class="isian-formulir"><input type="text" name="penerbit"
+                        value="<?php echo $r_tampil_buku['penerbit']; ?>" class="isian-formulir isian-formulir-border">
+                </td>
+            </tr>
+            <tr>
+                <td class="label-formulir">Status</td>
+                <td class="isian-formulir"><input type="text" name="status"
+                        value="<?php echo $r_tampil_buku['status']; ?>" class="isian-formulir isian-formulir-border">
+                </td>
+            </tr>
+            <!-- <tr>
+                <td class="label-formulir">Status</td>
+                <td class="isian-formulir"><input type="radio" name="status" value="Tersedia">Tersedia</label></td>
+            </tr>
+            <tr>
+                <td class="label-formulir"></td>
+                <td class="isian-formulir"><input type="radio" name="status" value="Dipinjam">Dipinjam</td>
+            </tr> -->
+            <tr>
+                <td class="label-formulir"></td>
+                <td class="isian-formulir"><input type="submit" name="simpan" value="Edit" class="tombol"></td>
+            </tr>
+        </table>
+    </form>
+</div>
